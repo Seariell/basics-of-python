4e099cad234d506a7b4204b85ec6c1e3d87e49f0
Nataliya Odeeva <nataus@yandex.ru>
2020-01-23 20:40:16 +0300

    ДЗ 1

---
+++ lesson-1-01.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+print("a =", a)
+print("b =", b)
+
+number1 = int(input("Введите число: "))
+print("Вы ввели: ", number1)
+number2 = int(input("Введите число: "))
+print("Вы ввели: ", number2)
+string1 = input("Введите строку: ")
+print("Вы вели: ", string1)
+string2 = input("Введите строку: ")
+print("Вы ввели: ", string2)
